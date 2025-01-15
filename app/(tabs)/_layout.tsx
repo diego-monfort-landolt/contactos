99903df644e317e7b2d9fd3c0dc3427f4b0615ef
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-01-15 13:00:25 +0100

    Hinzufügen von das der header nicht angezeigt wird

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
-import { DarkTheme } from '@react-navigation/native'
 
 const TabsLayout = () => {
   return (
@@ -13,6 +12,7 @@ const TabsLayout = () => {
         tabBarIcon: ({ color, size }) => (
           <Ionicons name='home' color={color} size={size}/>
         ),
+        headerShown: false
       }}
       />
 
@@ -20,6 +20,7 @@ const TabsLayout = () => {
         tabBarIcon: ({ color, size }) => (
           <Ionicons name='add' color={color} size={size}/>
         ),
+        headerShown: false
       }}
       />
 
@@ -27,6 +28,7 @@ const TabsLayout = () => {
         tabBarIcon: ({ color, size }) => (
           <Ionicons name='person' color={color} size={size}/>
         ),
+        headerShown: false
       }}
       />
     </Tabs>
