ff5ea3ab9d61b5db778d4ab7be94398ce84c5227
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-01-16 19:27:03 +0100

    Bearbeiten Home seite oder start seite alles mittig

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
 
 const Index = () => {
   return (
-   <View style={styles.container}>
-         <Text style={styles.headline}>Mi Agenda</Text>
-         <Link href="/(tabs)/home" style={styles.navLinks}>Mi Libreta</Link>
-         
-       </View>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.headline}>Mi Agenda</Text>
+      <Link href="/(tabs)/home" style={styles.navLinks}>Mi Libreta</Link>
+    </View>
+  );
+};
 
-export default Index
+export default Index;
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
     textAlign: 'center',
-    marginTop: 100,
   },
   headline: {
     fontSize: 25,
@@ -29,19 +28,21 @@ const styles = StyleSheet.create({
     letterSpacing: 2,
   },
   navLinks: {
-    color: 'white', 
+    color: 'white',
     padding: 10,
-    backgroundColor: '#007BFF', // Ein schönes Blau 
+    backgroundColor: '#007BFF', // Ein schönes Blau
     fontSize: 20,
-    textAlign: 'center', 
-    width: 200, 
-    borderRadius: 10, 
-    borderWidth: 0, 
-    shadowColor: '#000', 
-    shadowOffset: { 
-      width: 0, 
-      height: 4 
-    }, 
-    shadowOpacity: 0.1, shadowRadius: 6, elevation: 3
-  }
-})
+    textAlign: 'center',
+    width: 200,
+    borderRadius: 10,
+    borderWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 3,
+  },
+});
