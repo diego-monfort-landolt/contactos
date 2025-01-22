c249bad11762e6fdf8f99d2a9859301ef6bccb6d
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-01-22 20:57:08 +0100

    Hinzufügen eines hitnergrund bildes der index - es wird im vollem display angezeigt auch wird unter dem knopf und dem text ein leicht schwarzer hintergrund plaziert um den text lesbar zu machen

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 
 const Index = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.headline}>Mi Agenda</Text>
-      <Link href="/(tabs)/home" style={styles.navLinks}>Mi Libreta</Link>
-    </View>
+    <ImageBackground
+      source={{ uri: 'https://cdn.pixabay.com/photo/2021/11/30/00/54/sweden-6834164_640.jpg' }}
+      style={styles.background}
+      imageStyle={styles.backgroundImage}
+    >
+      <View style={styles.overlay}>
+        <Text style={styles.headline}>Mi Agenda</Text>
+        <Link href="/(tabs)/home" style={styles.navLinks}>Mi Libreta</Link>
+      </View>
+    </ImageBackground>
   );
 };
 export default Index;
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    
+  },
+  backgroundImage: {
+    resizeMode: 'cover', 
+    aspectRatio: 1, 
+  },
+  overlay: {
     padding: 20,
-    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparente para mejorar la visibilidad del texto y botones
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 3,
   },
   headline: {
     fontSize: 25,
     textAlign: 'center',
     marginBottom: 20,
     letterSpacing: 2,
+    color: 'white', // Asegura que el texto sea visible
   },
   navLinks: {
     color: 'white',
     padding: 10,
-    backgroundColor: '#007BFF', // Ein schönes Blau
+    backgroundColor: '#007BFF', // Un bonito azul
     fontSize: 20,
     textAlign: 'center',
     width: 200,
